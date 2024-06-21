27554665e9939b28feb406e9ccdfddc39e39077d
Aman raj <73831510+amansd25@users.noreply.github.com>
2024-06-21 10:38:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("./models/product.model.js");
+const productRoute = require("./routes/product.route.js");
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+
+// routes
+app.use("/api/products", productRoute);
+
+
+
+
+app.get("/", (req, res) => {
+  res.send("Hello from Node API Server Updated");
+});
+
+
+mongoose
+  .connect(
+    "mongodb+srv://ar4431734:dunQj2wezt1g3e7x@cluster0.aj20rcq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    console.log("Connected to database!");
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
